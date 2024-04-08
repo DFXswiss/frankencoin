2a3616d803f90ca63f54941f9a5c8352241b92fd
David May <david.leo.may@gmail.com>
2024-04-08 09:51:43 +0200

    [DEV-2433] Added FPS

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,12 @@
 import { Asset, Blockchain } from '@dfx.swiss/react';
 
 export const AvailableTokens: { [c in Blockchain]?: string[] } = {
-  [Blockchain.ETHEREUM]: ['ZCHF', 'ETH', 'WBTC', 'USDT'],
-  [Blockchain.POLYGON]: ['ZCHF', 'MATIC', 'WBTC', 'USDT'],
-  [Blockchain.OPTIMISM]: ['ZCHF', 'ETH', 'WBTC', 'USDT'],
-  [Blockchain.ARBITRUM]: ['ZCHF', 'ETH', 'WBTC', 'USDT'],
-  [Blockchain.BINANCE_SMART_CHAIN]: ['ZCHF', 'BNB', 'BTCB', 'USDT'],
-  [Blockchain.BASE]: ['ZCHF', 'ETH'],
+  [Blockchain.ETHEREUM]: ['ZCHF', 'ETH', 'WBTC', 'USDT', 'FPS'],
+  [Blockchain.POLYGON]: ['ZCHF', 'MATIC', 'WBTC', 'USDT', 'WFPS'],
+  [Blockchain.OPTIMISM]: ['ZCHF', 'ETH', 'WBTC', 'USDT', 'WFPS'],
+  [Blockchain.ARBITRUM]: ['ZCHF', 'ETH', 'WBTC', 'USDT', 'WFPS'],
+  [Blockchain.BINANCE_SMART_CHAIN]: ['ZCHF', 'BNB', 'BTCB', 'USDT', 'WFPS'],
+  [Blockchain.BASE]: ['ZCHF', 'ETH', 'WFPS'],
 };
 export const AvailableChains = Object.keys(AvailableTokens) as Blockchain[];
 
